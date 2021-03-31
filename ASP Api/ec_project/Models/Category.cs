ca522d31d228cd3c4fb7bcf34d35f916f9a81aee
nguyenhoanglinh601 <nguyenhoanglinh308@gmail.com>
2021-03-31 16:25:19 +0700

    create api server

---
+++ ASP Api/ec_project/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ec_project.Models
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ ASP Api/ec_project/Services/AdminService.cs
@@ -0,0 +1,44 @@
+﻿using ec_project.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ec_project.Services
+{
+    public class AdminService
+    {
+        private readonly IMongoCollection<Admin> _admins;
+
+        public AdminService(IECProjectDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _admins = database.GetCollection<Admin>(settings.AdminsCollectionName);
+        }
+
+        public List<Admin> Get() =>
+            _admins.Find(admin => true).ToList();
+
+        public Admin Get(string id) =>
+            _admins.Find<Admin>(admin => admin._id == id).FirstOrDefault();
+
+        public Admin Create(Admin admin)
+        {
+            _admins.InsertOne(admin);
+            return admin;
+        }
+
+        public void Update(string id, Admin adminIn) =>
+            _admins.ReplaceOne(admin => admin._id == id, adminIn);
+
+        public void Remove(Admin adminIn) =>
+            _admins.DeleteOne(admin => admin._id == adminIn._id);
+
+        public void Remove(string id) =>
+            _admins.DeleteOne(admin => admin._id == id);
+    }
+}
+
